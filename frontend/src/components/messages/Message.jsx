598a4585a58207928c4eab435679fa3352303940
Manoj-Rishi <manojrishi777@gmail.com>
2024-10-20 19:58:36 +0530

    3rd part completed

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
+import { useAuthContext } from '../../context/AuthContext';
+import userConversation from '../../zustand/useConversation';
+import { extractTime } from '../../utils/extractTime';
 
-const Message = () => {
+const Message = ({message}) => {
+    const {authUser} = useAuthContext();
+    const {selectedConversation} = userConversation();
+    const fromMe = message.senderId === authUser._id;
+    const formattedTime = extractTime(message.createdAt);
+
+
+    const chatClassName = fromMe ? 'chat-end' : 'chat-start';
+    const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+    const bubbleBgColor = fromMe ? 'bg-blue-500' : "";
+
+
   return (
-    <div className='chat chat-end'>
+    <div className={`chat ${chatClassName}`}>
         <div className='chat-image avatar'>
             <div className='w-10 rounded-full'>
                 <img 
                     alt='Chat bubble component'
-                    src={
-                        "https://avatar.iran.liara.run/public/39"
-                    }
+                    src={profilePic}
                 />
             </div>
         </div>
-        <div className={'chat-bubble text-white bg-blue-500'}> Hi! What's up?</div>
-        <div className='chat-footer opacity-60 text-xs flex gap-1 items-center'>3:00</div>
+        <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
+        <div className='chat-footer opacity-60 text-xs flex gap-1 items-center'>
+            {formattedTime}
+        </div>
     </div>
   );
 };
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Conversation from './Conversation';
+import useGetConversations from '../../hooks/useGetConversations';
+import { getRandomEmoji } from '../../utils/emojis';
 
 const Conversations = () => {
+  const {loading, conversations } = useGetConversations();
+  console.log("Conversations:",conversations);
   return (
     <div className='py-2 flex flex-col overflow-auto'>
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
+      {conversations.map((conversation,idx) => ( 
+        <Conversation 
+          key={conversation._id} 
+          conversation={conversation} 
+          emoji={getRandomEmoji()}
+          lastIdx = {idx === conversation.length-1} // doesn't show divider for last chat in sidebar
+        />
+      ))}
+        {loading ? <span className='loading loading-spinner mx-auto'></span> : null}
     </div>
   );
 };
